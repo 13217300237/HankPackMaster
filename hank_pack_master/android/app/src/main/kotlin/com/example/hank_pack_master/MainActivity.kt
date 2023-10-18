069839b121ffafc8885815d46429fb5d01dc7d82
周航 <545679452@qq.com>
2023-10-18 23:54:35 +0800

    init project

---
+++ hank_pack_master/android/app/src/main/kotlin/com/example/hank_pack_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hank_pack_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hank_pack_master/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('''
+              这是一个流水线作业工具，它目前的功能只针对安卓工程:
+              1. 调用git指令克隆git仓库，并切换到指定分支，并且合并指定分支的代码
+              2. 执行gradle命令，对安卓工程进行编译，随后执行打包指令，最后产出apk文件
+              3. 执行签名指令，对apk进行再签名
+              4. 执行加固命令，对apk进行加固并再签名
+              5. 将加固后的apk文件上传到 pgy 这种托管平台
+              其中各种指令的执行结果，通过stream 显示在日志面板上
+              
+              我做这个的目的，就是将打包出产物的流程一体化，人为的操作只有 设置好 git仓库以及 工作分支然后点击开始打包即可。
+              生成的产物apk将会以 下载链接，下载二维码，以及 文件本身的方式，呈现在 用户设置好的目标目录上
+              ''',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.dangerous),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
