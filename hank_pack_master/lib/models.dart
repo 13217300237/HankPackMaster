a572e6cec7e3b019c2d4d01719eb6b64f6da366b
18598925736 <545679452@qq.com>
2023-12-29 16:21:36 +0800

    Update models.dart

--- hank_pack_master/lib/models.dart
+++ hank_pack_master/lib/models.dart
@@ -3,6 +3,10 @@ import 'package:flutter/cupertino.dart';
 class ExecutorModel extends ChangeNotifier {
   List<String> lines = [];
 
+  final ScrollController _scrollController = ScrollController();
+
+  ScrollController get scrollController => _scrollController;
+
   List<String> get getRes => lines;
 
   reset() {
@@ -13,5 +17,10 @@ class ExecutorModel extends ChangeNotifier {
   append(String res) {
     lines.add(res);
     notifyListeners();
+    scrollToBottom();
+  }
+
+  void scrollToBottom() {
+    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
   }
 }
