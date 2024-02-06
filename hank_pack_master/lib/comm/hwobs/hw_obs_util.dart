7829e95e764d2c103f1baa6bdf6a8e7ee774d52d
18598925736 <zhouhang64@h-partners.com>
2024-02-06 15:46:17 +0800

    鉴权计算的问题解决

--- hank_pack_master/lib/comm/hwobs/hw_obs_util.dart
+++ hank_pack_master/lib/comm/hwobs/hw_obs_util.dart
@@ -48,25 +48,23 @@ class HwObsUtil {
 
     String contentMD5 =
         ""; // 提前计算出要上传对象的MD5，在上传完成之后服务端会进行校验，如果不同，会告知客户端，传输过程中遇到风险了
-    String contentType = "multipart/form-data";
+    String contentType = "";
     String canonicalizedHeaders = "";
     String canonicalizedResource = "/$_bucketName/objecttest1";
     debugPrint("canonicalizedResource: $canonicalizedResource");
     String stringToSign =
         "$method\n$contentMD5\n$contentType\n$requestTime\n$canonicalizedHeaders$canonicalizedResource";
-    debugPrint("StringToSign: $stringToSign");
-
+    debugPrint("加密前的原文:\n$stringToSign");
+    debugPrint("加密用的sk是:\n$_sk");
     // 2. 使用SK对StringToSign UTF-8编码之后的结果进行HMAC-SHA1签名计算
     List<int> keyBytes = utf8.encode(_sk);
     List<int> messageBytes =
         utf8.encode(stringToSign); // 对 StringToSign 进行 UTF-8 编码
     Hmac hmacSha1 = Hmac(sha1, keyBytes);
     Digest hmacDigest = hmacSha1.convert(messageBytes);
-    String hmacSha1Result = hmacDigest.toString();
-    debugPrint("HMAC-SHA1计算的结果是:  $hmacSha1Result");
-
     // 3. 对第3步的结果进行Base64编码
-    String encodedMessage = base64.encode(utf8.encode(hmacSha1Result));
+    String encodedMessage = base64.encode(hmacDigest.bytes);
+
     debugPrint("最终签名: OBS $_ak:$encodedMessage"); // 打印编码后的字符串
 
     return 'OBS $_ak:$encodedMessage';
@@ -82,21 +80,24 @@ class HwObsUtil {
   /// 调用 OBS上传，必须关闭XGate，不然网络有问题
   doUpload() async {
     try {
-      String requestTime = nowDate();
-      String authorization = getAuthorization('post', requestTime);
+      String requestTime = 'Tue, 06 Feb 2024 07:40:05 GMT'; // TODO nowDate(); 记得改回来，
       String url = "https://$_bucketName.$_endpoint";
       debugPrint("请求地址为：$url");
 
-      // 构建 FormData
+      // 构建 FormData 请求体
       FormData formData = FormData.fromMap({
         "file": await MultipartFile.fromFile('D:\\OBSobject\\text01.txt'),
         "key": "text01.txt",
       });
 
+      // 请求头
       var options = Options(
         headers: {
           'Date': requestTime,
-          'Authorization': authorization,
+          'Host': '_bucketName.$_endpoint',
+          'contentType': "text/plain",
+          'canonicalizedResource': "/$_bucketName/objecttest1",
+          'Authorization': getAuthorization('PUT', requestTime),
         },
       );
 
@@ -134,5 +135,6 @@ class HwObsUtil {
   }
 
 // 官方计算的: OBS WME9RK9W2EA5J7WMG0ZD:Ha1fA/9wR0qIxXhGw7mBAJO46xM=
+//         OBS WME9RK9W2EA5J7WMG0ZD:eyoAYE6MT1hgg+rVE/Ee82u8eYQ=
 // 本人计算的: OBS WME9RK9W2EA5J7WMG0ZD:MWRhZDVmMDNmZjcwNDc0YTg4YzU3ODQ2YzNiOTgxMDA5M2I4ZWIxMw==
 }
